8d06b4038d190da26330236b26433097abd3908a
Arthur <arthur.pocholle@gmail.com>
2024-10-21 09:39:41 +0200

    exercice1

---
+++ exercice1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void init_grille(int grille[9][9], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            grille[i][j] = 0;
+        }
+    }
+}
+
+int main() {
+    int grille[9][9];
+    int n=6;
+    init_grille(grille, n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", grille[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
