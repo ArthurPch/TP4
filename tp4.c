fb35c7cc4a2209f82b94a09c9cab40e7be3d7942
Arthur <arthur.pocholle@gmail.com>
2024-10-23 18:43:05 +0200

    ajout de l'exercice 7( dans le dernier push je ne sais pas pourquoi mais mon remplirGrille a disparu)

--- tp4.c
+++ tp4.c
@@ -89,27 +89,95 @@ void generer_grille_aleatoire(int grille[9][9], int n, int niveau) {
     }
 }
 
+int remplirGrille(int grille[9][9], int n) {
+    int colonne, ligne, chiffre;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            while (grille[i][j] == 0) {
+                printf("Mettre le numero de la colonne (0 a %d), de la ligne (0 a %d) et le chiffre souhaite (1 a %d) : ", n-1, n-1, n);
+                scanf("%d %d %d", &colonne, &ligne, &chiffre);
+
+                if (colonne >= 0 && colonne < n && ligne >= 0 && ligne < n && chiffre >= 1 && chiffre <= n) {
+                    grille[ligne][colonne] = chiffre;
+
+                    if (verificationGrille(grille, n) == 0) {
+                        afficher_grille(grille, n);
+                    } else {
+                        printf("La grille contient des erreurs. Veuillez corriger.\n");
+                        grille[ligne][colonne] = 0;
+                    }
+                } else {
+                    printf("Erreur : Colonne, ligne ou chiffre invalides. Veuillez reessayer.\n");
+                }
+            }
+        }
+    }
+    return 0;    
+}
+
 int main() {
-    int grille1[9][9];
-    int grille2[9][9];
-    int grille3[9][9];
+    int grille[9][9];
     int n = 4;
+    int choix=0, niveau;
 
-    init_grille(grille1, n);
-    init_grille(grille2, n);
-    init_grille(grille3, n);
+    while (choix != 6) {
+        printf("\nMenu :\n");
+        printf("1. Choisir la taille de la grille\n");
+        printf("2. Initialiser la grille\n");
+        printf("3. Generer la grille aleatoirement\n");
+        printf("4. Resoudre la grille manuellement\n");
+        printf("5. Afficher la grille\n");
+        printf("6. Quitter\n");
+        printf("Choisissez une option : ");
+        scanf("%d", &choix);
 
-    printf("Grille avec moitie des cases remplies (niveau 1) :\n");
-    generer_grille_aleatoire(grille1, n, 1);
-    afficher_grille(grille1, n);
+        switch (choix) {
+            case 1:                
+                printf("Entrez la taille de la grille (4 ou 9) : ");
+                scanf("%d", &n);
+                if (n != 4 && n != 9) {
+                    printf("Taille invalide. Veuillez choisir 4 ou 9.\n");
+                    n = 9; 
+                }
+                init_grille(grille, n);
+                printf("Grille de taille %dx%d choisie.\n", n, n);
+                break;
 
-    printf("\nGrille avec un tiers des cases remplies (niveau 2) :\n");
-    generer_grille_aleatoire(grille2, n, 2);
-    afficher_grille(grille2, n);
+            case 2:               
+                init_grille(grille, n);
+                printf("Grille initialisee.\n");
+                break;
 
-    printf("\nGrille avec un quart des cases remplies (niveau 3) :\n");
-    generer_grille_aleatoire(grille3, n, 3);
-    afficher_grille(grille3, n);
+            case 3:                
+                printf("Choisissez le niveau de generation (1=moitie, 2=tiers, 3=quart) : ");
+                scanf("%d", &niveau);
+                if (niveau < 1 || niveau > 3) {
+                    printf("Niveau invalide. Choix par defaut : niveau 1.\n");
+                    niveau = 1;
+                }
+                generer_grille_aleatoire(grille, n, niveau);
+                printf("Grille generee aleatoirement avec le niveau %d.\n", niveau);
+                afficher_grille(grille, n);
+                break;
+
+            case 4:                
+                remplirGrille(grille, n);
+                afficher_grille(grille, n);
+                break;
+
+            case 5:
+                afficher_grille(grille, n);
+                break;
+
+            case 6:
+                printf("Quitter le programme.\n");
+                break;
+
+            default:
+                printf("Option invalide. Veuillez reessayer.\n");
+        }
+    }
 
     return 0;
 }
